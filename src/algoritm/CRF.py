3d146a2f848cd7a5499052bc0a99d17c1ee14361
pyli <pyli@ronglian.com>
2019-01-02 13:59:23 +0800

    添加crf的修改

--- src/algoritm/CRF.py
+++ src/algoritm/CRF.py
@@ -4,10 +4,13 @@
 import copy
 import numpy as np
 import random
+import itertools
 
 class LinearChainCRF():
     
-    def __init__(self):
+    def __init__(self, learningRate = 0.001, epoch = 1):
+        self.learningRate = 0.001
+        self.epoch = epoch
         #定义CRF的参数
         #特征模板。如果没有设置，就是用使用默认的线性链条件随机场的模板规则，即只考虑当前
         #观测值x_t，当前隐藏状态y_t，以及前一个隐藏状态y_before_t
@@ -16,50 +19,25 @@ class LinearChainCRF():
         self.minCharNum = None#语料中的字符，出现次数小于这个阈值的，不会用于模板
         self.gradListOfWeights = None#训练过程中，存储各个模板函数权重的梯度
         self.learningRate = None#模型训练的学习率，这里为了简单，使用一个统一的
-        self.statTransMap = None#存储隐藏状态之间的转换
         self.hiddenStateNum = None
-        self.statObserveMap = None#存储各个隐藏状态与观测值
-        self.weightOfFeatures =  None#每一个模板函数的权重
+        self.featureWeightMap =  None#每一个模板函数的权重,为了减小激素啊过程的内存消耗，这里使用map存储
+        self.stateTransFeatureSet = None
+        self.stateFeatureSet = None
         self.featureFunctionNum = None#特征函数的个数
-        self.featureFunctionNameIndex = None#存储每一个特征函数在权重向量中的位置
-        self.hiddenStateNameIndex = None#存储每一个状态的序号
         #为了简单，这里暂时不加正则化
 
-    #基于模板规则，计算各个特征函数，在各个观测值处的取值
-    def calFeatureFunctionValueArray4LinearChainCRF(self, stateList, observationList):
-        featureFunctionValue = np.zeros(self.featureFunctionNum)#每一个特征函数的取值
-        charList = observationList
-        tagList = stateList
-        lengthOfSentence = len(charList)
-        # print(stateList)
-        # print(observationList)
-        for i in range(1, lengthOfSentence-1):#遍历观测序列上的每一个观测值
-            statTransFeature = tagList[i-1:i+1]#线性链CRF的特征函数只有两种
-            statFeature = tagList[i] + charList[i]
-            # print('asd',statTransFeature)
-            indexOfStatTransFeature = self.featureFunctionNameIndex[statTransFeature]
-            # print(featureFunctionValue)
-            featureFunctionValue[indexOfStatTransFeature] += self.ifFitStatTransFeatureTemplet(statTransFeature)
-            indexOfStatFeature = self.featureFunctionNameIndex[statFeature]
-            featureFunctionValue[indexOfStatFeature] += self.ifFitStatTransFeatureTemplet(statTransFeature)
-        return featureFunctionValue
-
     #判断当前状态转换是否符合某一个特征模板，返回值为这个特征函数的取值
-    def ifFitStatTransFeatureTemplet(self, statTransFeature):
-        if statTransFeature in self.statTransMap:
-            return 1.
-        else:
-            return 0.
-    def ifFitStatFeatureTemplet(self, statFeature):
-        if statFeature in self.statObserveMap:
+    def ifFitFeatureTemplet(self, statTransFeature):
+        if statTransFeature in self.stateTransFeatureSet or statTransFeature in self.stateFeatureSet:
             return 1.
         else:
             return 0.
 
     #基于超参数和训练数据，初始化CRF的参数
     def initParamWithTraingData(self, traningData):
-        self.statTransMap, self.statObserveMap = {}, {}
-        self.featureFunctionNameIndex = {}
+        self.featureWeightMap = {}
+        stateTransFeatureNumMap, statFeatureNumMap = {}, {}
+        self.stateTransFeatureSet, self.stateFeatureSet = None, None
         hiddenStateSet = set({})
         for sentence in traningData:
             charList = '@' + sentence[0] + '@'
@@ -70,60 +48,54 @@ class LinearChainCRF():
             for i in range(1, sentenceLength-1):
                 statTransFeature = tagList[i - 1: i+1]  # 线性链CRF的特征函数只有两种
                 statFeature = tagList[i] + charList[i]
-                self.statTransMap[statTransFeature] = self.statTransMap.get(statTransFeature, 0) + 1
-                self.statObserveMap[statFeature] = self.statObserveMap.get(statFeature, 0) + 1
-        print("可用的状态转移有", self.statTransMap)
-        print("隐藏状态-观测值:",self.statObserveMap)
-        featureNameList = list(self.statTransMap.keys()) + list(self.statObserveMap.keys())
+                #统计特征在语料中出现的次数，如果太低，就删掉
+                stateTransFeatureNumMap[statTransFeature] = stateTransFeatureNumMap.get(statTransFeature, 0) + 1
+                statFeatureNumMap[statFeature] = statFeatureNumMap.get(statFeature, 0) + 1
+        for feature in list(stateTransFeatureNumMap.keys()):
+            if stateTransFeatureNumMap[feature]<2:
+                del stateTransFeatureNumMap[feature]
+            else:#出现次数较高的特征，给一个初始权重
+                self.featureWeightMap[feature] = random.uniform(-0.1, 0.1)
+        for feature in list(statFeatureNumMap.keys()):
+            if statFeatureNumMap[feature]<2:
+                del statFeatureNumMap[feature]
+            else:#出现次数较高的特征，给一个初始权重
+                self.featureWeightMap[feature] = random.uniform(-0.1, 0.1)
+        self.stateTransFeatureSet = set(list(stateTransFeatureNumMap.keys()))
+        self.stateFeatureSet = set(list(statFeatureNumMap.keys()))
+        print("特征函数的初始权重是", self.featureWeightMap)
+        featureNameList = list(self.featureWeightMap.keys())
         self.featureFunctionNum = len(featureNameList)
         self.hiddenStatList = list(hiddenStateSet)
         self.hiddenStateNum = len(hiddenStateSet)
-        self.weightOfFeatures = np.array([random.uniform(-0.1, 0.1) for _ in range(self.featureFunctionNum)])
-        for i in range(self.featureFunctionNum):
-            # if featureNameList[i]=='@第':
-            #     print(featureNameList[i])
-            self.featureFunctionNameIndex[featureNameList[i]] = i
         print("特征函数的个数是", self.featureFunctionNum)
 
-    #已知CRF参数，以及一个观测序列，计算一个隐藏状态序列的概率
-    def calConditionalProbOfStates(self, stateList, observationList):
-        observationList = '@' + observationList + '@'#为首位和末尾观测值的模板添加占位符号
-        stateList = '*' + stateList+ '#'
-        lengthOfSentence = len(stateList)
-        featureValueArray = self.calFeatureFunctionValueArray4LinearChainCRF(stateList, observationList)
-        numerator = np.exp(sum(self.weightOfFeatures*featureValueArray))
+    #计算观测序列t处，某个特征函数的取值，以及对应的边缘概率，为计算梯度做准备
+    def calFeatureFunctionValueAndMargProb(self, featureName, observationList, t):
+        margProb = 0
+        if featureName in self.stateFeatureSet:#如果是状态特征
+            state_t = featureName[0]#状态特征的name的第一位是stat_t, 第二位是x_t
+            margProb = self.forwardAlgrithm(state_t, observationList, t) * \
+                               self.backwardAlgrithm(state_t, observationList, t)
+        elif featureName in self.stateTransFeatureSet:#如果是状态转移特征
+            observation_t = observationList[t]
+            state_t = featureName[1]
+            stateFormer = featureName[0]
+            margProb = self.forwardAlgrithm(stateFormer, observationList, t-1) * \
+                             self.getSumOfFeatureFuctions(state_t, stateFormer, observation_t) * \
+                               self.backwardAlgrithm(state_t, observationList, t)
+        return margProb
+            
+            
 
-        #用前向算法，计算分布Z(x)
-        scoreOfEachStateChain = np.ones(self.hiddenStateNum)#以某个隐藏状态结尾的
-        tempResList = [copy.deepcopy(scoreOfEachStateChain) for _ in range(lengthOfSentence)]
-        #state序列，对应的特征函数取值和
-        for i in range(1, len(stateList)-1):#遍历每一个观测值和隐藏状态
-            scoreOfLastStep = tempResList[i-1]#上一步一算得到的，各组路径的得分和
-            print(scoreOfLastStep)
-            scoreOfThisStep = tempResList[i]#这一步，各组路径的得分和
-            for j in range(self.hiddenStateNum):#遍历每一个隐藏状态取值
-                thisState = self.hiddenStatList[j]#这个状态的取值
-                for n in range(self.hiddenStateNum):#遍历上一步的所有路径的得分
-                    lastState = self.hiddenStatList[n]#上一步的一个状态的取值
-                    stateTrans = lastState + thisState
-                    stateTransFeatureValue = self.ifFitStatTransFeatureTemplet(stateTrans)
-                    scoreOfThisStep[j] += scoreOfLastStep[n] * np.exp(stateTransFeatureValue)
-                thisObservation = observationList[i]
-                stateFeature = thisState + thisObservation
-                scoreOfThisStep[j] *= np.exp(self.ifFitStatFeatureTemplet(stateFeature))
-        denominator = np.sum(tempResList[-2])
-        prob = numerator/denominator
-        print("条件概率的分子是", numerator, '分母是',denominator, prob)
-        return prob
-
+    #已知模型参数，求一个观测值处的特征函数加权和
     def getSumOfFeatureFuctions(self, thisState, formerState, thisObservation):
-        formerState = formerState
         stateTrans = formerState + thisState
         stateFeature = formerState + thisObservation
-        featureValue = self.weightOfFeatures[self.hiddenStateNameIndex[stateTrans]] * \
-                       self.ifFitStatTransFeatureTemplet(stateTrans) + \
-                       self.weightOfFeatures[self.hiddenStateNameIndex[stateFeature]] * \
-                       self.ifFitStatTransFeatureTemplet(stateFeature)
+        featureValue = self.featureWeightMap.get(stateTrans, 0) * \
+                       self.ifFitFeatureTemplet(stateTrans) + \
+                       self.featureWeightMap.get(stateFeature, 0) * \
+                       self.ifFitFeatureTemplet(stateFeature)
         return featureValue
 
     #已知CRF模型参数和一个观测序列x=(x_1, x_2, ..., x_T)，
@@ -140,7 +112,7 @@ class LinearChainCRF():
             for n in range(len(self.hiddenStatList)):#遍历本步的所有隐藏状态
                 thisState = self.hiddenStatList[n]
                 for j in range(stateNumOfFormerStep):
-                    formerState = self.hiddenStateNameIndex[j]
+                    formerState = self.hiddenStatList[j]
                     featureFunctionSum = self.getSumOfFeatureFuctions(thisState, formerState, thisObservation)
                     thisAlpha[n] += np.exp(featureFunctionSum) * formerAlpha[j]
             alphaList.append(thisAlpha)
@@ -151,7 +123,7 @@ class LinearChainCRF():
         stateNumOfFormerStep = len(formerAlpha)
         alpha_t = 0#x_t处，隐藏状态取值为state_t时的前向变量取值
         for j in range(stateNumOfFormerStep):
-            formerState = self.hiddenStateNameIndex[j]
+            formerState = self.hiddenStatList[j]
             featureFunctionSum = self.getSumOfFeatureFuctions(thisState, formerState, thisObservation)
             alpha_t += np.exp(featureFunctionSum) * formerAlpha[j]
         return  alpha_t
@@ -162,7 +134,7 @@ class LinearChainCRF():
         betaList = [None for _ in range(len(observationList))]#存储前向向量
         betaList[-1] = [1]#认为添加的start步，也就是t=0的位置，对应的状态只有一个，前向向量
         #的长度就是1,特征函数的个数是0,向量元素的取值就是exp(0)=1
-        for i in range(len(observationList) - 2, t+1, -1):
+        for i in range(len(observationList) - 2, t, -1):
             thisObservation = observationList[i]
             thisBeta= np.zeros(self.hiddenStateNum)#t>=1时，隐藏状态的个数
             formerBeta = betaList[i + 1]
@@ -170,36 +142,86 @@ class LinearChainCRF():
             for n in range(len(self.hiddenStatList)):#遍历本步的所有隐藏状态
                 thisState = self.hiddenStatList[n]
                 for j in range(stateNumOfFormerStep):
-                    formerState = self.hiddenStateNameIndex[j]
+                    formerState = self.hiddenStatList[j]
                     featureFunctionSum = self.getSumOfFeatureFuctions(thisState, formerState, thisObservation)
                     thisBeta[n] += np.exp(featureFunctionSum) * formerBeta[j]
             betaList[i] = thisBeta
 
         thisState = state_t
         thisObservation = observationList[t]
-        formerAlpha = betaList[t+1]
-        stateNumOfFormerStep = len(formerAlpha)
+#         print(t, len(betaList), betaList)
+        laterAlpha = betaList[t+1]
+        stateNumOfFormerStep = len(laterAlpha)
         beta_t = 0#x_t处，隐藏状态取值为state_t时的前向变量取值
         for j in range(stateNumOfFormerStep):
-            formerState = self.hiddenStateNameIndex[j]
+            formerState = self.hiddenStatList[j]
             featureFunctionSum = self.getSumOfFeatureFuctions(thisState, formerState, thisObservation)
-            beta_t += np.exp(featureFunctionSum) * formerAlpha[j]
+            beta_t += np.exp(featureFunctionSum) * laterAlpha[j]
         return beta_t
 
+    def generatePossibleStateTrans(self, t):#生成第t步，可能的隐藏状态转移
+        featureNames = []
+        if t==1:
+            featureNames = itertools.product(['*'], self.hiddenStatList)
+        else:
+            featureNames = itertools.product(self.hiddenStatList, self.hiddenStatList)
+        featureNames = list(filter(lambda x: x in self.featureWeightMap, featureNames))
+        return featureNames
+    
+    def generatePossibleStateFeatueNames(self, t, observation):
+        featureNames = []
+        if t==1:
+            featureNames = itertools.product(['@'], [observation])
+        else:
+            featureNames = itertools.product(self.hiddenStatList, [observation])
+        featureNames = list(filter(lambda x: x in self.featureWeightMap, featureNames))
+        return featureNames
+        
     #计算模板函数权重对应的梯度
     def calGrad4Weight(self, sentence):
-        pass
-
+        charList = '@' + sentence[0] + '@'
+        tagList = '*' + sentence[1] + '#'
+        sentenceLength = len(charList)
+        z_x = self.backwardAlgrithm('*', charList, 0)#计算配分函数的取值
+        gradMap = {}
+        for t in range(1, sentenceLength-1):
+            thisState, formerState = tagList[t], tagList[t-1]
+            thisObservation = charList[t]
+            featureName1 = formerState + thisState
+            if featureName1 in self.featureWeightMap:
+                gradMap[featureName1]  = gradMap.get(featureName1, 0) + self.ifFitFeatureTemplet(featureName1)
+                possibleFeatureNames = self.generatePossibleStateFeatueNames(t, thisObservation)
+                for featureName in possibleFeatureNames:
+                    gradMap[featureName1] -= self.calFeatureFunctionValueAndMargProb(featureName, charList, t)/z_x
+                
+            featureName2 = thisState + thisObservation
+            if featureName2 in self.featureWeightMap:
+                gradMap[featureName2]  = gradMap.get(featureName2, 0) + self.ifFitFeatureTemplet(featureName2)
+                possibleFeatureNames = self.generatePossibleStateTrans(t)
+                for featureName in possibleFeatureNames:
+                    gradMap[featureName2] -= self.calFeatureFunctionValueAndMargProb(featureName, charList, t)/z_x                      
+            
+        return gradMap
+       
     # 基于更新规则更新权重
-    def updateWeight(self):
-        pass
+    def updateWeight(self, gradMap):
+        for featureName in gradMap:
+#             print(self.featureWeightMap.keys())
+            if featureName in self.featureWeightMap:
+                self.featureWeightMap[featureName] -= 0.01 * gradMap[featureName]
+#             else:
+#                 print(featureName)
 
     #基于训练语料，估计CRF参数
     def fit(self, sentenceList):
         self.initParamWithTraingData(sentenceList)
-        # for sentence in sentenceList:#遍历语料中的每一句话，训练模型
-        #     self.calGrad4Weight(sentence)#计算模板函数权重对应的梯度
-        #     self.updateWeight()#基于更新规则更新权重
+        for _ in range(self.epoch):
+            for sentence in sentenceList:#遍历语料中的每一句话，训练模型
+                gradMap = self.calGrad4Weight(sentence)#计算模板函数权重对应的梯度
+    #             print("梯度是", list(gradMap.items()))
+                self.updateWeight(gradMap)#基于更新规则更新权重
+    #             print("更新后的权重是", list(self.featureWeightMap.items())[:10])
+                print(self.featureWeightMap['ES'])
             
     #基于观测值序列，也就是语句话的字符串列表，使用模型选出最好的隐藏状态序列，并按照分词标记将字符聚合成分词结果
     def predict(self, text): 
@@ -298,7 +320,7 @@ def loadData(fileName, sentenceNum = 100):
             else:
                 line= line.split('\t')
 #                 print(line)
-                [char, tag] = line[0], line[1]#取出语料的文字和分词标记
+                [char, tag] = line[0], line[2]#取出语料的文字和分词标记
                 tempSentence.append(char)
                 tempTag.append(tag)
             line = f.readline()
@@ -306,16 +328,16 @@ def loadData(fileName, sentenceNum = 100):
                       
 import time
 if __name__ == '__main__':
-    fileName = r"trainingCorpus4wordSeg_part.txt"
+    fileName = r"msra_training.txt"
     sentenceNum = 50
     sentenceList = loadData(fileName, sentenceNum=sentenceNum)#加载语料
-    print(sentenceList)
-    model = LinearChainCRF()
+#     print(sentenceList)
+    model = LinearChainCRF(epoch=10)
     model.fit(sentenceList)
-    print("一个隐藏状态序列的概率是",
-          sentenceList[0],
-          model.calConditionalProbOfStates(sentenceList[0][1],
-                                                           sentenceList[0][0]))
+#     print("一个隐藏状态序列的概率是",
+#           sentenceList[0],
+#           model.calConditionalProbOfStates(sentenceList[0][1],
+#                                                            sentenceList[0][0]))
     # res = model.predict(sentenceList[10][0])
     # print("分词结果是", res, "真实的分词结果是", )
     #
