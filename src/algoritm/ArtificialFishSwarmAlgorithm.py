6d1fb0649cc2ff1da97119af308da2f62e3e61bc
lipengyu <lipy@founder.com.cn>
2019-09-17 09:34:55 +0800

    动画展示参数巡游过程

--- src/algoritm/ArtificialFishSwarmAlgorithm.py
+++ src/algoritm/ArtificialFishSwarmAlgorithm.py
@@ -5,6 +5,8 @@ Created on 2019年9月11日
 '''
 #人工鱼群算法
 import numpy as np
+import matplotlib.pyplot as plt
+
 #第一阶段的人工鱼群只有追寻食物的机制。只让人工鱼寻找食物浓度最高的位置，就可以实现对一些的损失函数的优化了。
 class AFish():
     
@@ -20,14 +22,13 @@ class AFSA():
         self.bulletin_fish = None
         self.visual = visual#所有鱼的视野范围。可以为每条鱼设置不同的视野大小，模拟个体差异
         self.try_num_searching_food = try_num_searching_food#多试几次有助于找到更好的方向。
-        self.max_fish_in_vision = 10#视野内鱼的最大个数，用于限制鱼的密度
+        self.max_fish_in_vision = 5#视野内鱼的最大个数，用于限制鱼的密度
         self.fish_swarms = {}
         self.create_fishes(fish_num, location_dim)
 
         
     def food_density(self, location):
-#         print('location', location[0])
-        score = -(np.sum(location*location) + 2*location[1])#z=x**2+y**2 +3.105*y- 1,需要加一个符号，让函数是凸的。如果需要优化的目标函数比较复杂，就没有这么直观了。
+        score = -(np.sum(location*location) + 2*location[1])#z=x**2+y**2 +2*y,需要加一个符号，让函数是凸的。如果需要优化的目标函数比较复杂，就没有这么直观了。
         return score
     
     def distance(self, location1, location2):
@@ -105,15 +106,37 @@ class AFSA():
         self.random_move(fish)#假如鱼没有觅食、追尾或者聚群，就随机移动一下。
         
     def fit(self ,epoch_num=1000):
+        
+        #########将寻优过程可视化，不是必须的#########
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(1,1,1)
+        x = list(map(lambda x: x.location[0], self.fishes))
+        y = list(map(lambda x: x.location[1], self.fishes))
+        self.ax.scatter(x, y)
+        plt.ion()
+        ############可视化部分####################
+        
         for epoch in range(epoch_num):
             for fish in self.fishes:
                 self.update_a_fish(fish)
             print("轮次是", epoch)
             print(self.bulletin_fish.location)
+            
+            self.show_locations()#可视化
 #             break
-                
-    
+
+    #把所有鱼的位置变化展示出来，参考了https://blog.csdn.net/omodao1/article/details/81223240
+    def show_locations(self):
+        x = list(map(lambda x: x.location[0], self.fishes))
+        y = list(map(lambda x: x.location[1], self.fishes))
+        try:
+            self.ax.lines.remove(self.lines[0])
+        except Exception as e:
+            print(e)
+        self.lines = self.ax.plot(x ,y, '*')
+        plt.pause(0.1)
+
 if __name__ == '__main__':
     afsa = AFSA()
-    afsa.fit()
+    afsa.fit(epoch_num=1000)
         
