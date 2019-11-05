3554c3d4254fd5fec21384745a79649b41c0b887
lipengyu <lipy@founder.com.cn>
2019-11-05 09:52:31 +0800

    比较直观的simhash实现

--- src/nlp/simhash.py
+++ src/nlp/simhash.py
@@ -1,6 +1,5 @@
 '''
 Created on 2019年11月4日
-
 @author: Administrator
 '''
 #使用simhash计算文本相似度，并去重
@@ -10,9 +9,6 @@ import numpy as np
 
 class SimHash():
     
-    def __init__(self):
-        self.doc_id_simhash_map  =None#存储所有文章的id和simhash码
-            
     def segment(self, text):
         words = []
         for word_tag in HanLP.segment(text):
@@ -23,13 +19,14 @@ class SimHash():
         words = self.segment(text)
         simhash_code = np.zeros(64)
         for word in words:
-            simhash_code += self.get_word_code(word)
-#         print(simhash_code)
-        for i in range(simhash_code.shape[0]):
+            simhash_code += self.word_binary_hash(word)
+#             simhash_code += self.string_hash(word)
+            
+        for i in range(64):
             if simhash_code[i]>=0:
                 simhash_code[i]=1
             else:
-                simhash_code[0]=0
+                simhash_code[i]=0
                 
         return simhash_code
     
@@ -51,25 +48,53 @@ class SimHash():
 #             print(binary_code)
             four_bit_code = binary_code#'0'*(4-len(binary_code)) + binary_code
             bin_code += four_bit_code
-        return bin_code
+#         print(bin_code)
+        code = np.ones(64)
+        for i in range(64):
+            if bin_code[i]=='0':
+                code[i] = -1
+        return code
     
     def get_word_code(self, word):
         word_code = np.ones(64)
-        binary_code = self.word_binary_hash(word)
+        binary_code = self.string_hash(word)
         for i in range(64):
+#             print(binary_code[binary_index])
             if binary_code[i]=="0":
                 word_code[i] = -1
+#         print(word_code)
         return word_code
     
-    
+    def string_hash(self,source):
+        code = np.ones(64)
+        if source == "":
+            return code
+        else:
+            x = ord(source[0]) << 7
+            m = 1000003
+            mask = 2 ** 128 - 1
+            for c in source:
+                x = ((x * m) ^ ord(c)) & mask
+            x ^= len(source)
+            if x == -1:
+                x = -2
+            x = bin(x).replace('0b', '').zfill(64)[-64:]
+#                 print(source,x)
+        for i in range(len(x)):
+            if x[i]=='0':
+                code[i] = -1
+#         print(code)  
+        return code
+             
 corpus = ["我要上厕所", "我要上个厕所", "我要去上厕所了"]
-corpus = list(open(r"C:\Users\Administrator\Desktop\简单任务\算法学习\simhash\corpus.txt", 'r', encoding='utf8'))
+corpus = list(open(r"c:\Users\lipy\Desktop\new 18", 'r', encoding='utf8'))
 if __name__ == '__main__':
     a = SimHash()
+#     print(a.get_simhash_code("我"))
     for i in range(len(corpus)):
         for j in range(i, len(corpus)):
             distance = a.get_distance(corpus[i], corpus[j])
-            print(i, j, distance)
+            print(i+1, j+1, distance)
         
         
         
