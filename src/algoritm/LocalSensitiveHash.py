2acf62a73a690ac583acc7d9cdc153acbea07033
lipengyu <lipy@founder.com.cn>
2019-11-12 14:57:37 +0800

    添加注释

--- src/algoritm/LocalSensitiveHash.py
+++ src/algoritm/LocalSensitiveHash.py
@@ -20,7 +20,8 @@ class RandomProjectionHash():
         self.char_id_map = {}
         for i in range(len(chars)): self.char_id_map[chars[i]] = i
 
-    def random_projection_hash(self, text):
+    #原始的随机投影hash算法，计算得到的结果是浮点数，允许使用欧氏距离来计算文本距离。这样有溢出风险；浮点数导致我们无法把数据放到“桶”里，不利于检索。
+    def ori_random_projection_hash(self, text):
         lsh_code = np.zeros(self.K)
         for i in range(self.K):
             tf = np.zeros(self.vocab_size)
@@ -28,12 +29,26 @@ class RandomProjectionHash():
                 index = self.char_id_map[char]
                 tf[index] += 1
             dot_res = np.dot(tf, self.random_vector[i, :])
+            lsh_code[i] = dot_res
+        return lsh_code
+    
+    #将随机投影hash的结果转换为0和1，使之可以用来对数据进行简单的聚合；并支持快速的相似度计算。点被限制在了一个象限内， 分不会比较密集，对下有任务不太有利。
+    def new_random_projection_hash(self, text):
+        lsh_code = np.zeros(self.K)
+        for i in range(self.K):
+            tf = np.zeros(self.vocab_size)
+            for char in text:
+                index = self.char_id_map[char]
+                tf[index] += 1
+            dot_res = np.dot(tf, self.random_vector[i, :])
+            dot_res
             if dot_res>0:
                 lsh_code[i] = 1
             else:
                 lsh_code[i] = 0
         return lsh_code
-
+    
+    #将随机投影法得到的浮点数转化为-1和1，增强了hash码的语义表现能力
     def random_hyperplane_hash(self, text):
         lsh_code = np.zeros(self.K)
         for i in range(self.K):
@@ -55,14 +70,15 @@ class RandomProjectionHash():
             if code1[i]==code2[i]:
                 score += 1
         return score/len(code1)
-    
+
 if __name__ == '__main__':
     RPHash = RandomProjectionHash()
     s = 'i am chinese asd asd  asd asd asd asd '
     code1 = RPHash.random_hyperplane_hash(s)
     print(code1)
+    print('ori_random_projection_hash', RPHash.ori_random_projection_hash(s))
     s = 'i am a chinese asd asd  asd asd asd asd '
-    code2 = RPHash.random_hyperplane_hash(s)
+    code2 = RPHash.new_random_projection_hash(s)
     print(code2)
     print("相似度是", RPHash.simlarity(code1, code2))
     print("http://www.apabi.cn/ontologies/xisixiang#中国\u2014委内瑞拉高级混合委员会")
